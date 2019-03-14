1397f230750ae2d0d4ace1808a1d15ff0e07f151
Kevin Li <kevinli020508@gmail.com>
2019-03-14 06:14:04 -0400

    Add $.contextMenu plugin to create context menu in textarea

---
+++ js/contextMenu.js
@@ -0,0 +1,19 @@
+// Free up the "$" symbol for custom function
+jQuery.noConflict();
+(function( $ ) {
+    $(function() {
+        $.contextMenu({
+            selector: '#FARTextarea', 
+            callback: function(key, options) {
+                var m = "clicked: " + key;
+                window.console && console.log(m) || alert(m); 
+            },
+            items: {
+                "copy": {name: "Copy", icon: "copy"},
+                "paste": {name: "Paste", icon: "paste"},
+                "cut": {name: "Cut", icon: "cut"},
+                "delete": {name: "Delete", icon: "delete"},
+            }
+        });
+    });
+  })(jQuery);
