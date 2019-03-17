84339393f43550a2946270ebdde43519121dfc11
Kevin Li <kevinli020508@gmail.com>
2019-03-18 03:42:04 +0800

    Minify css for production and introduce terser-webpack-plugin to minimize es6 files

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,19 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const TerserPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = merge(common, {
     mode: 'production',
-    devtool: 'souce-map'
+    devtool: 'souce-map',
+    optimization: {
+        minimizer: [
+          new TerserPlugin({
+            cache: true,
+            parallel: true,
+            sourceMap: true // set to true if you want JS source maps
+          }),
+          new OptimizeCSSAssetsPlugin({})
+        ]
+      },
 });
