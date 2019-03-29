9f1949427746c273e0c574bdb3540a0a735d9717
Kevin Li <kevinli020508@gmail.com>
2019-03-29 09:53:55 +0800

    Remove context menu and related dependencies

--- src/script.js
+++ src/script.js
@@ -1,9 +1,5 @@
-import "./SmartTextareaBase.js"
-import "./ContextMenuMixin.js"
+import "./SmartTextarea.js"
 
-// Initialize smartTextarea
-window.SmartTextarea = class SmartTextarea extends ContextMenuMixin(SmartTextareaBase){
-
-}
 // const smartTextarea1 = new SmartTextarea(document.getElementById("textarea1"));
 // const smartTextarea2 = new SmartTextarea(document.getElementById("textarea2"));
+// const smartTextarea3 = new SmartTextarea(document.getElementById("textarea3"));
